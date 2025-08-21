aba72470bdf3f6adb471588ee6a8e7600550b0fd
codingHisla <mohammadbabaye8989@gmail.com>
2025-08-21 23:01:40 +0330

    feat: add check box functionality and improve ui that's a real step forward

--- src/todolist.ts
+++ src/todolist.ts
@@ -44,6 +44,7 @@ interface Todo {
   category: string;
   priority: string;
   date: string;
+  status: boolean;
   description?: string;
 }
 
@@ -69,6 +70,7 @@ function addTodo(event: Event): void {
 
   const todo: Todo = {
     id: todosArray.length,
+    status: false,
     title: todoNameinput.value,
     category: category.value,
     date: date.value ? date.value : new Date().toLocaleDateString("en-CA"),
@@ -81,112 +83,94 @@ function addTodo(event: Event): void {
   clearInput();
 }
 function appendTodo(): void {
-  let taskComponent: string = `` as string;
+  let taskComponent: string = ``;
   todoWrapper.innerHTML = "";
   todosArray.forEach((item: Todo) => {
-    taskComponent += `<div
-            class="bg-subInput border bordesr-black-border rounded-default min-w-[74rem] px-[7rem] py-[3rem] my-9"
-          >
-            <div class="flex items-center justify-between">
-              <div class="flex gap-[7px] items-center">
-                <label
-                  class="relative inline-block pl-9 mb-1 cursor-pointer text-base select-none mt-[-1.2rem] -ml-12"
-                >
-                  <input
-                    type="checkbox"
-                    class="peer absolute opacity-0 h-0 w-0 cursor-pointer"
-                  />
-                  <span
-                    class="absolute top-0 left-0 h-6 w-6 bg-gray-200 rounded transition-all duration-300 shadow-md peer-checked:bg-lightColor peer-checked:shadow-lg peer-checked:shadow-secbg-lightColor after:content-[''] after:absolute after:left-2 after:top-0.5 after:w-2 after:h-3.5 after:border-white after:border-r-2 after:border-b-2 after:rotate-45 after:opacity-0 after:transition-opacity peer-checked:after:opacity-100"
-                  >
-                  </span>
-                </label>
-                <h3 class="font-bold text-3xl">${item.title}</h3>
-              </div>
-              <button
-                class="rounded-default bg-red-400 py-3 px-6 cursor-pointer text-white transition-colors hover:bg-red-700 border border-black-border"
-                onclick="deleteTodo(${item.id})"
+    taskComponent += `
+      <div
+        class="bg-subInput border ${
+          !item.status ? "border-red-400" : "border-green-400"
+        } rounded-default min-w-[74rem] px-[7rem] py-[3rem] my-9 transition-all"
+      >
+        <div class="flex items-center justify-between">
+          <div class="flex gap-[7px] items-center">
+            <label
+              class="relative inline-block pl-9 mb-1 cursor-pointer text-base select-none mt-[-1.2rem] -ml-12"
+            >
+              <input
+                type="checkbox"
+                class="peer absolute opacity-0 h-0 w-0 cursor-pointer todo-check"
+                data-id="${item.id}"
+                ${item.status ? "checked" : ""}
+              />
+              <span
+                class="absolute top-0 left-0 h-6 w-6 bg-gray-200 rounded transition-all duration-300 shadow-md peer-checked:bg-green-400 peer-checked:shadow-lg peer-checked:shadow-secbg-green-400 after:content-[''] after:absolute after:left-2 after:top-0.5 after:w-2 after:h-3.5 after:border-white after:border-r-2 after:border-b-2 after:rotate-45 after:opacity-0 after:transition-opacity peer-checked:after:opacity-100"
               >
-                Delete
-              </button>
-            </div>
-            <p class="my-3">
-             ${item.description}
-            </p>
-            <div class="flex items-center gap-10">
-              <div class="flex items-center gap-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="size-7"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-tag w-4 h-4 text-gray-400"
-                >
-                  <path
-                    d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z"
-                  ></path>
-                  <circle
-                    cx="7.5"
-                    cy="7.5"
-                    r=".5"
-                    fill="currentColor"
-                  ></circle>
-                </svg>
-                <span>${item.category}</span>
-              </div>
-              <div class="flex items-center gap-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="size-7"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-flag w-3 h-3"
-                >
-                  <path
-                    d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"
-                  ></path>
-                  <line x1="4" x2="4" y1="22" y2="15"></line>
-                </svg>
-                <span>${item.priority}</span>
-              </div>
-              <div class="flex items-center gap-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="size-7"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-calendar w-4 h-4"
-                >
-                  <path d="M8 2v4"></path>
-                  <path d="M16 2v4"></path>
-                  <rect width="18" height="18" x="3" y="4" rx="2"></rect>
-                  <path d="M3 10h18"></path>
-                </svg>
-                <span>${item.date}</span>
-              </div>
-            </div>
+              </span>
+            </label>
+            <h3 class="font-bold text-3xl ${
+              item.status ? "line-through text-gray-400" : ""
+            }">
+              ${item.title}
+            </h3>
           </div>
-`;
+          <button
+            class="rounded-default bg-red-400 py-3 px-6 cursor-pointer text-white transition-colors hover:bg-red-700 border border-black-border"
+            onclick="deleteTodo(${item.id})"
+          >
+            Delete
+          </button>
+        </div>
+        <p class="my-3">
+          ${item.description || ""}
+        </p>
+        <div class="flex items-center gap-10">
+          <div class="flex items-center gap-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z"/>
+              <circle cx="7.5" cy="7.5" r=".5" fill="currentColor"/>
+            </svg>
+            <span>${item.category}</span>
+          </div>
+          <div class="flex items-center gap-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"/>
+              <line x1="4" x2="4" y1="22" y2="15"/>
+            </svg>
+            <span>${item.priority}</span>
+          </div>
+          <div class="flex items-center gap-4">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-7 text-gray-400" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M8 2v4"/>
+              <path d="M16 2v4"/>
+              <rect width="18" height="18" x="3" y="4" rx="2"/>
+              <path d="M3 10h18"/>
+            </svg>
+            <span>${item.date}</span>
+          </div>
+        </div>
+      </div>
+    `;
   });
+
   todoWrapper.insertAdjacentHTML("beforeend", taskComponent);
+
+  // وصل کردن event به همه checkbox ها
+  const checkboxes =
+    todoWrapper.querySelectorAll<HTMLInputElement>(".todo-check");
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener("change", (event) => {
+      const id = Number(checkbox.dataset.id);
+      changeTodoStatus(event, id);
+    });
+  });
 }
+
 function clearInput(): void {
   description.value = "";
   todoNameinput.value = "";
 }
-function deleteTodo(todoId): void {
+function deleteTodo(todoId: number): void {
   todosArray = todosArray.filter((todo: Todo) => todo.id !== todoId);
 
   checkHasTodo();
@@ -202,6 +186,17 @@ function checkHasTodo(): void {
     todoWrapper.classList.remove("hidden");
     taskContainerTitle.classList.remove("hidden");
   }
+}
+
+function changeTodoStatus(event: Event, todoId: number): void {
+  const input = event.target as HTMLInputElement;
+  console.log(event);
+
+  todosArray = todosArray.map((todo) =>
+    todo.id === todoId ? { ...todo, status: input.checked } : todo
+  );
+
+  appendTodo();
 }
 
 todoNameinput.addEventListener("click", showingForm);
