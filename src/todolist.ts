efa5a4641c41ae6b28431224d74eeb055d1ca75f
codingHisla <mohammadbabaye8989@gmail.com>
2025-08-21 11:58:55 +0330

    feat: add ts to dependencies

--- src/todolist.ts
+++ src/todolist.ts
@@ -4,6 +4,12 @@ const todoForm = $.querySelector("#form") as HTMLFormElement;
 const todoNameinput = $.querySelector("#todoNameInput") as HTMLInputElement;
 const closeTodoBtn = $.querySelector("#cancelBtn") as HTMLButtonElement;
 const openTodoBtn = $.querySelector("#openBtn") as HTMLButtonElement;
+// use whenever we didn't have task
+const noTaskContainer = $.querySelector("#noTask") as HTMLDivElement;
+
+const addTaskBtn = $.querySelector("#addTaskBtn") as HTMLButtonElement;
+
+let taskComponent: string = `` as string;
 
 function showingForm(): void {
   todoForm.classList.add("active");
@@ -14,7 +20,8 @@ function closingForm(event: Event): void {
   todoNameinput.value = "" as string;
   todoForm.classList.remove("active");
 }
-
+function addTodo(event: Event) {}
 todoNameinput.addEventListener("click", showingForm);
 openTodoBtn.addEventListener("click", closingForm);
 closeTodoBtn.addEventListener("click", (event) => closingForm(event));
+addTaskBtn.addEventListener("click", (event) => addTodo(event));
